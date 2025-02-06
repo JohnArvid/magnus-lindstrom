c4327ec0d5781608351ae7774fbf1598803af541
Arvid Lindström <arvid-lindstrom@live.se>
2025-02-06 20:20:17 +0100

    basic stuff added

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,6 @@
 'use strict';
-console.log('main.js running');
+let year = new Date().getFullYear();
+let span = document.createElement("span");
+span.textContent = ` ${year}`;
+let footerHead = document.querySelector("footer h3");
+footerHead.appendChild(span);
